e3842b86d9d3fae6066b3d762171f5e6ae455321
trienntp65cntt-crypto <trien.ntp.65cntt@ntu.edu.vn>
2026-03-15 23:02:58 +0700

    th2_2

--- BaiTap/appCong/app/src/main/java/com/example/appcong/MainActivity.java
+++ BaiTap/appCong/app/src/main/java/com/example/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void XuLyCong(View view){
+        EditText editTextSoA = findViewById(R.id.edta);
+        EditText editTextSoB  = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+
+        String strA =   editTextSoA.getText().toString();
+        String strB =   editTextSoB.getText().toString();
+
+        //chuyen dl sang so
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+        //tinh tong
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+        // in ra mh
+        editTextKetQua.setText(strTong);
     }
 }
