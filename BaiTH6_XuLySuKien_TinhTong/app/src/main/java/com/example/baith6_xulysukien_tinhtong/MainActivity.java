a88c6a91e52a65c8272cf5896c5bf1c5eeb93a07
trienntp65cntt-crypto <trien.ntp.65cntt@ntu.edu.vn>
2026-03-21 22:27:45 +0700

    hoàn thành

---
+++ BaiTH6_XuLySuKien_TinhTong/app/src/main/java/com/example/baith6_xulysukien_tinhtong/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.baith6_xulysukien_tinhtong;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    // day la bo xu ly cong
+    public void XuLyCong(View view){
+        EditText editTextSoA = findViewById(R.id.edta);
+        EditText editTextSoB  = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+
+        String strA =   editTextSoA.getText().toString();
+        String strB =   editTextSoB.getText().toString();
+
+        //chuyen dl sang so
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+        //tinh tong
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+        // in ra mh
+        editTextKetQua.setText(strTong);
+    }
+}
